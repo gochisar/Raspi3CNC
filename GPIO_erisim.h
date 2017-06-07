805f1c89ba1f1b7a075f18991cdc9a7bcddd6e81
Hakan Eroğlu <hakaneroglu@gmail.com>
2017-06-07 08:36:07 +0300

    ek fonksiyonlar ile erişim değiştirildi
    pin giriş çıkış ayarlamaları ve giriş çıkış değerleri yollama fonksiyonları eklendi.

--- GPIO_erisim.h
+++ GPIO_erisim.h
@@ -2,7 +2,7 @@
 #define GPIO_erisim_h
 
 #define BCM2708_PERI_BASE        0x3F000000
-#define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
+#define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO kontrol adresi */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,6 +13,7 @@
 #define PAGE_SIZE (4*1024)
 #define BLOCK_SIZE (4*1024)
  
+bool io_set = false;
 int  mem_fd;
 void *gpio_map;
  
@@ -45,33 +46,51 @@ void printButton(int g)
 //
 void setup_io()
 {
-   /* open /dev/mem */
-   if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
-      printf("can't open /dev/mem \n");
-      exit(-1);
-   }
+	if(!io_set){
+		/* open /dev/mem */
+		if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
+			printf("can't open /dev/mem \n");
+			exit(-1);
+		}
  
-   /* mmap GPIO */
-   gpio_map = mmap(
-      NULL,             //Any adddress in our space will do
-      BLOCK_SIZE,       //Map length
-      PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
-      MAP_SHARED,       //Shared with other processes
-      mem_fd,           //File to map
-      GPIO_BASE         //Offset to GPIO peripheral
-   );
+		/* mmap GPIO */
+		gpio_map = mmap(
+			NULL,             //Any adddress in our space will do
+			BLOCK_SIZE,       //Map length
+			PROT_READ|PROT_WRITE,// Enable reading & writting to mapped memory
+			MAP_SHARED,       //Shared with other processes
+			mem_fd,           //File to map
+			GPIO_BASE         //Offset to GPIO peripheral
+		);
  
-   close(mem_fd); //No need to keep mem_fd open after mmap
+		close(mem_fd); //No need to keep mem_fd open after mmap
  
-   if (gpio_map == MAP_FAILED) {
-      printf("mmap error %d\n", (int)gpio_map);//errno also set!
-      exit(-1);
-   }
+		if (gpio_map == MAP_FAILED) {
+			printf("mmap error %d\n", (int)gpio_map);//errno also set!
+			exit(-1);
+		}
  
-   // Always use volatile pointer!
-   gpio = (volatile unsigned *)gpio_map;
- 
+		// Always use volatile pointer!
+		gpio = (volatile unsigned *)gpio_map;
+		io_set = true;
+	}
  
 } // setup_io
 
+void pin_cikis(int n){
+	INP_GPIO(n);
+	OUT_GPIO(n);
+}
+
+void pin_giris(int n){
+	INP_GPIO(n);
+}
+
+void pin_set(int n){
+	GPIO_SET = 1<<n;
+}
+
+void pin_clr(int n){
+	GPIO_CLR = 1<<n;
+}
 #endif
